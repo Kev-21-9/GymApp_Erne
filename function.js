b73ce50aba5abfe56c3b6e824ab2802542114870
Kev-21-9 <102910433+Kev-21-9@users.noreply.github.com>
2022-05-23 16:26:11 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,17 @@
-function addWorkout(){
-    workoutlist.innerHTML += `
-    <li> ${workoutfield.value} </li>
-    `;
+const formula = document.getElementById("formula");
+formula.addEventListener('submit', newWorkout);
+
+function newWorkout(event){
+    event.preventDefault()
+    const input = document.getElementById("workoutfield");
+    displayElement.textContent = input;
+}
+
+var buttons = document.querySelectorAll('button');
+for(var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', openNewTab, false);
+}
+
+function openNewTab(event) {
+   window.open(event.target.dataset.url, '_blank');
 }
